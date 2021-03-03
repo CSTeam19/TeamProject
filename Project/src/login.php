29abb07569873af242633ddf525a7bc813980597
Taoufik Laaroussi <mr.l.t@hotmail.com>
2021-03-03 17:27:23 -0500

    Add new features to login page

--- Project/src/login.php
+++ Project/src/login.php
@@ -1,36 +1,64 @@
 <?php
 
-require_once 'session.php';
-require_once 'db_connect.php';
+ $inputFromJson = json_decode(file_get_contents('php://input'), true);
 
-if(isset($_POST['login-btn'])) {
+    $email = $inputFromJson['userName'];
+    $password = $inputFromJson['password'];
+    $sql;
 
-    $user = $_POST['user-name'];
-    $password = $_POST['user-pass'];
+     //CONNECTING to SQL server
+    $dbServerName = "localhost";
+    $dbUserName = "root";
+    $dbPassword = "toorqwer";
+    $dbName = "pricereviewdb";
 
-    try {
-      $SQLQuery = "SELECT * FROM customer WHERE customer_email = :useremail";
-      $statement = $conn->prepare($SQLQuery);
-      $statement->execute(array(':useremail' => $user));
-
-      while($row = $statement->fetch()) {
-        $hashed_password = $row['customer_password'];
-        $username = $row['customer_name'];
-        $useremail = $row['customer_email'];
-
-        if(password_verify($password, $hashed_password)) {
-          $_SESSION['username'] = $username;
-          header('location: dashboard.php');
+    $conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
+    
+    //Start Reading Sequence
+        if ($conn->connect_error)
+        {
+            error( $conn->connect_error);
         }
-        else {
-          echo "Error: Invalid username or password";
+        else{
+            //query to DB
+            $sql = "SELECT customer_id, customer_name, customer_prof_status FROM customer WHERE customer_email = '".$email."' and customer_password = '".$password."';";
+            $result = mysqli_query($conn, $sql);
+            $numRows = mysqli_num_rows($result);
+            //Review SQL Result
+            if($numRows > 0){
+                //User found
+                $user = $result->fetch_assoc();
+                $id = $user["customer_id"];
+                $fullName = $user["customer_name"];
+                $profile_status = $user["customer_prof_status"];
+                //echo ($id);
+                returnUser($id, $fullName, $profile_status);
+
+            }
+            //User not found
+            else{
+                error("Email or password is incorrect");
+                }
+            $conn->close();
         }
-      }
-    }
-    catch (PDOException $e) {
-      echo "Error: " . $e->getMessage();
+    //FUNCTIONS
+
+    function error($err){
+        $result = '{"customer_id":0, "error":"' . $err . '"}';
+        toJSON($result);
     }
 
-}
+    //This takes the user to the landing page 
+    //It will also send the user info to the landing page
+    function returnUser($id, $fullName, $profile_status){
+        $ret = '{"customer_id":"'. $id .'", "customer_fullname": "'. $fullName .'", "customer_prof_status": "'. $profile_status .'"}';
+        toJSON($ret);
+    }
 
+    //This return JSON files to JS
+    function toJSON($json){
+        header('Content-type: application/json');
+    echo $json;
+    }
+        
 ?>
--- Project/src/signup.php
+++ Project/src/signup.php
@@ -1,31 +1,59 @@
 <?php
 
-require_once 'db_connect.php';
+require_once ('sendmail.php');
+  $inputFromJson = json_decode(file_get_contents('php://input'), true);
 
-if(isset($_POST['signup-btn'])) {
+    $fullName = $inputFromJson['fullName'];
+    $password =  $inputFromJson['password'];
+    $phoneNumber = $inputFromJson['phoneNumber'];
+    $email = $inputFromJson['email'];
+    $sql;
 
-      $username = $_POST['user-name'];
-      $email = $_POST['user-email'];
-      $password = $_POST['user-pass'];
+     //CONNECTING to SQL server
+    $dbServerName = "localhost";
+    $dbUserName = "root";
+    $dbPassword = "toorqwer";
+    $dbName = "pricereviewdb";
+    
+    $confirmCode = rand(100000, 1000000);
 
-      $hashed_password = password_hash($password, PASSWORD_DEFAULT);
-
-    try {
-      $SQLInsert = "INSERT INTO customer (customer_email, customer_name, customer_password, customer_prof_status)
-                   VALUES (:email, :username, :password, 0)";
-
-      $statement = $conn->prepare($SQLInsert);
-      $statement->execute(array(':username' => $username, ':password' => $hashed_password, ':email' => $email));
-
-      if($statement->rowCount() == 1) {
-        header('location: ../login.html');
-        echo "User added successfully!<br>";
-      }
-    }
-    catch (PDOException $e) {
-      echo "Error: " . $e->getMessage();
-    }
-
-}
-
-?>
+    $conn = mysqli_connect($dbServerName, $dbUserName, $dbPassword, $dbName);
+    
+    //Start Reading Sequence
+        if ($conn->connect_error)
+        {
+            error( $conn->connect_error);
+        }
+       else
+       {
+        $sql = "INSERT INTO customer (customer_name, customer_password, customer_phone, customer_email, customer_prof_status, customer_confcode) 
+        VALUES ('".$fullName."','".$password."','".$phoneNumber."','".$email."', 0, '".$confirmCode."');";
+    
+        if($conn->query($sql) != TRUE )
+        {
+          returnError( $conn->error );
+        }
+        else
+        {
+          sendEmail($email, $confirmCode);
+          returnInfo("done");
+        }
+        $conn->close();
+      }    
+    
+  function returnError($error){
+        $retval = '{"msg":"' . $error .'"}';
+    outputJson($retval);
+  }
+  
+  function returnInfo($info){
+        $retval = '{"msg":"' . $info .'"}';
+    outputJson($retval);
+  }
+  
+  function outputJson ($file){
+    header("Content-type:application/json");
+    echo $file;
+  }
+  
+  
